1b6bf940c334eab72b1eb82160527a25fd9bc75d
Haomogg <385371785@qq.com>
2024-03-22 23:34:47 +0800

    Delete the gamma relevant variants.

--- src/hk_camera.cpp
+++ src/hk_camera.cpp
@@ -376,8 +376,8 @@ void HKCameraNodelet::reconfigCB(CameraConfig& config, uint32_t level)
     config.exposure_min = exposure_min_;
     config.gain_auto = gain_auto_;
     config.gain_value = gain_value_;
-    config.gamma_selector = gamma_selector_;
-    config.gamma_value = gamma_value_;
+//    config.gamma_selector = gamma_selector_;
+//    config.gamma_value = gamma_value_;
     config.white_auto = white_auto_;
     config.white_selector = white_selector_;
     config.stop_grab = stop_grab_;
@@ -395,7 +395,7 @@ void HKCameraNodelet::reconfigCB(CameraConfig& config, uint32_t level)
   {
     _MVCC_FLOATVALUE_T exposure_time;
 //    assert(MV_CC_SetIntValue(dev_handle_, "AutoExposureTimeLowerLimit", config.exposure_min) == MV_OK);
-    MV_CC_SetIntValue(dev_handle_, "AutoExposureTimeLowerLimit", config.exposure_min);
+    assert(MV_CC_SetIntValue(dev_handle_, "AutoExposureTimeLowerLimit", config.exposure_min) == MV_OK);
     assert(MV_CC_SetIntValue(dev_handle_, "AutoExposureTimeUpperLimit", config.exposure_max) == MV_OK);
     assert(MV_CC_SetEnumValue(dev_handle_, "ExposureAuto", MV_EXPOSURE_AUTO_MODE_CONTINUOUS) == MV_OK);
     assert(MV_CC_GetFloatValue(dev_handle_, "ExposureTime", &exposure_time) == MV_OK);
@@ -454,23 +454,6 @@ void HKCameraNodelet::reconfigCB(CameraConfig& config, uint32_t level)
     assert(MV_CC_SetEnumValue(dev_handle_, "BalanceWhiteAuto", MV_BALANCEWHITE_AUTO_OFF) == MV_OK);
     assert(MV_CC_GetIntValue(dev_handle_, "BalanceRatio", &white_value) == MV_OK);
     config.white_value = white_value.nCurValue;
-  }
-
-  switch (config.gamma_selector)
-  {
-    case 0:
-      assert(MV_CC_SetBoolValue(dev_handle_, "GammaEnable", true) == MV_OK);
-      assert(MV_CC_SetEnumValue(dev_handle_, "GammaSelector", MV_GAMMA_SELECTOR_SRGB) == MV_OK);
-      break;
-    case 1:
-      assert(MV_CC_SetBoolValue(dev_handle_, "GammaEnable", true) == MV_OK);
-      assert(MV_CC_SetEnumValue(dev_handle_, "GammaSelector", MV_GAMMA_SELECTOR_USER) == MV_OK);
-      assert(MV_CC_SetGamma(dev_handle_, config.gamma_value) == MV_OK);
-      break;
-    case 2:
-      assert(MV_CC_SetBoolValue(dev_handle_, "GammaEnable", false) == MV_OK);
-//      MV_CC_SetBoolValue(dev_handle_, "GammaEnable", false);
-      break;
   }
 
   take_photo_ = config.take_photo;
